b57aee2ca504e356bd139d5a502244badb58819d
anacamorims <ana.camorims.dev@gmail.com>
2024-04-09 22:48:39 -0400

    add: adding responsiveness and organizing indentation

--- pagina saiba mais/js/darkmode.js
+++ pagina saiba mais/js/darkmode.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const darkModePreference = localStorage.getItem('darkMode');
-  
-    // Função para aplicar o modo escuro
+  // Verifique se a preferência do modo escuro está armazenada no Local Storage
+  const darkModePreference = localStorage.getItem('darkMode');
+
+  // Função para aplicar o modo escuro
   function applyDarkMode() {
     document.body.classList.add('dark');
     const chk = document.getElementById('chk');
     chk.checked = true;
   }
-  
+
   // Função para remover o modo escuro
   function removeDarkMode() {
     document.body.classList.remove('dark');
     const chk = document.getElementById('chk');
     chk.checked = false;
   }
-  
+
   // Verifique se a preferência é "enabled"
   if (darkModePreference === 'enabled') {
     applyDarkMode();
   }
-  
+
   // Adicione o evento de mudança para alternar o modo escuro
   const chk = document.getElementById('chk');
   chk.addEventListener('change', () => {
@@ -31,13 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('darkMode', 'disabled');
     }
   });
-  
+
   // Verifique se a guia foi aberta por outra guia
   const isChildTab = window.opener ? true : false;
-  
+
   // Verifique se a preferência é "enabled" e a guia não é uma guia filha
   if (darkModePreference === 'enabled' && !isChildTab) {
-      applyDarkMode();
+    applyDarkMode();
   }
-
 });
